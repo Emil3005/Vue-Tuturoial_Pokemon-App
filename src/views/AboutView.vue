31da63fad14cd0e2ec28e65882697e73d4f97277
Emil Bartoldus <emilbartoldus@gmail.com>
2023-02-12 15:16:10 +0100

    Done

---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h3>{{ $route.params.slug }}</h3>
+  </div>
+</template>
+
+
+<script>
+import {useRoute} from "vue-router";
+import { reactive } from "vue";
+export default{
+
+  setup(){
+    const route = useRoute();
+
+    const state = reactive({
+      pokemon: null
+    })
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.slug}/`)
+    .then((res)=> res.json())
+    .then((data) =>{
+      console.log(data);
+    })
+  }
+
+}
+
+</script>
